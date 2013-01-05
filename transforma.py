70e2ab15885c13cb13bf431ce1532c4943108715
Félix Ortega Hortigüela <fortegah@gmail.com>
2013-01-05 21:19:57 +0100

    Primera version casi funcional

--- transforma.py
+++ transforma.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 import csv
 import os
 
 import re
 
-reject_arr = [  "C_Al", "areas57", "jenito", "kamal", "olaloka", "jose-m-m",
-				"ALFO.", "CristianDlx", "kikonuri", "raulillo.*", "pacohue",
-				"joru169", "teteluis", "wilkoazul", "manuelkle", "EDU95", 
-				"JAB.", "josemarias", "anlimo", "lynar+gh", "josemarias",
-				"anominos", "alameda", "FL0R", "honi", "stier", "tencas"
-				]
-reject_arr = map(lambda a: "(%s)" % a, reject_arr)
+reject_arr = [ "AJAULAR", "DEMAC", "MAURO", "RUBEN", "KESSELRIN", "MANR",
+				"NOMI", "RAFA", "ANGELRLOZANO", "ANONIMO", "AUGUSTO",
+				"LUISMI", "POLUZA", "JRLG", "TACK", "FLOR", "ABEL",
+				"FERNANDO", "EDU95", "BHST", "ORTEGA", "TONIWAN", "DAFF",
+				"DEME", "VILLAFORMER", "ORDAS", "YERA", "CROSGONZALEZ",
+				"CARLOS", "ANGEL", "CHUCHI", "FRANCISCO", "JMBH61", "LECH",
+				"MORADORES", "PILIS69", "RMTHX", "RUBIO", "SERR", "THOMSON",
+				"TXIKITXU", "RODRI", "NOVIAS", "MIES", "MART", "MAPA300",
+				"MACIA", "LFRODIS", "LEO", "GONZALO", "FAUS", "CARL", "CARM",
+				"COYO", "ZETA", "XAVI", "WING", "VAZQUEZ", "CRX", "Rubio",
+				"ANDER", "YETI22", "VAZFRAN", "TROPELIO", "TRIP", "TIVO",
+				"TIKIJAVI", "SUSPERRE", "RAUL", "RAMON", "PENADELAGUA",
+				"NAVARRETE", "NAPO", "NANO", "MIGUEL", "MIER", "MGM", "MANU",
+				"LFRODIS", "JRVB", "JOAQUINITO", "GUS", "GOTHMOG", "GIL",
+				"GAVALDA", "GAROFANO", "FMSG", "FL0R", "FITT", "ENEK", "CHEM",
+				"CAPARANAS", "BERE", "AZAYAS", "ARRA", "ARENAS", "ALFREDO",
+				"ACTUNGU2", "YETI", "ZENO", "VELOX", "TPOI" "TRAGULLA",
+				"PRORA", "PEPITO89", "PELEPAGES", "PAPI", "PAJE", "PADI",
+				"PACEXTREM", "OVERCRAFT", "OPSI", "NICOSAVE", "NEOZERO",
+				"NENE", "NCTF", "MIERES", "MOOR", "MRGYM", "ARBU", "ARVYDAS",
+				"AMAT", "AMARAL", "ALFO", "ANIESAJ", "AURE82", "ATIL",
+				"AUXIT", "BOIN", "BRAV", "BSHT", "COPI", "CHACON", "CESAR",
+				"CEMOS", "CARC", "DEANDRES", "DEY", "DINA","FCANTARERO",
+				"AKU26", "LUIS", "MASH", "LFRODRIS", "VIDA", "TRAGULLA",
+				"TPOI", "RUBI", "NICOSABE", "MICHAELKNIGHT", "MIANSUPA",
+				"MCBARTU", "MAURI", "MARSAMAR", "MAJE", "MAITE", "MARIO",
+				"LUISGA", "LOPEZ", "LOLE", "LLANO", "LIDAR", "LE", "KESEYO",
+				"KARLYSTYLE", "JUSANDE", "JUANS", "JUANCHI", "JUAN",
+				"JOSERRA", "JORG", "JOLU", "JLMC", "JJOSE", "JCERCENAL",
+				"JAVIFA", "JANT", "JALA", "JAB.", "INOXI", "HOTE", "HEINDAL",
+				"GORD", "FIBALOZ", "FHAGY", "FERNANDEZ", "FDEZRUIZ", "EPIL",
+				"\(repot.Outlook\)", "JCRCENAL",
+		]
+reject_arr = map(lambda a: "(?:%s)" % a, reject_arr)
 reject_exp = "|".join(reject_arr)
 reject = re.compile(reject_exp)
 
@@ -29,6 +58,13 @@ def transform_file(f, dst):
 			tipo = "tramo_final"
 		else:
 			tipo = "tramo_inicio"
+	elif tipo == "curvas":
+		# Castañaco, pasamos
+		return
+	elif tipo == "puntos":
+		# Castañaco, pasamos
+		return
+
 	
 	fd = open(f, "rb")
 	dialect = csv.Sniffer().sniff(fd.read(1024))
@@ -43,14 +79,12 @@ def transform_file(f, dst):
 	for a in csv.reader(fd, dialect):
 		longitud = a[0]
 		latitud = a[1]
-		comment = a[2].split(' ')
+		comment = a[2].split(' ')[3:]
 		comment = map(lambda a: a.strip(' @'), comment)
 
 		for el in comment:
-			if el.isupper() and el.isalpha():
-				comment = comment[1:]
-				continue
-
+			if re.match("[A-Z]+-[0-9]+", el):
+				break
 			if reject.match(el):
 				comment = comment[1:]
 				continue
@@ -98,16 +132,9 @@ def transform_file(f, dst):
 
 if __name__ == "__main__":
 	csv.register_dialect("misc", delimiter = ";", quoting = csv.QUOTE_ALL)
-	try: 
-		n_fich = int(sys.argv[1])
-	except:
-		n_fich = 99999999999
 
 	dst = csv.writer(open("radares.csv", "wb"), "misc")
-	c = 0
 	for a in os.listdir("datos"):
-		if c == n_fich:
-			fichero = os.path.join("datos", a)
-			print fichero
-			transform_file(fichero, dst)
-			c += 1
+		fichero = os.path.join("datos", a)
+		print fichero
+		transform_file(fichero, dst)
